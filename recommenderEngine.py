bcacc0043c3a28442b85ad7cd371d034a862d88c
Sheraz Tariq <sheraztariq@hotmail.com>
2016-09-07 05:36:40 -0400

    added byebug like feature

--- recommenderEngine.py
+++ recommenderEngine.py
@@ -1,4 +1,4 @@
-
+import pdb #byebug for python --> pdb.set_trace()
 from numpy import *
 
 num_movies = 10
@@ -7,21 +7,18 @@ num_users = 5
 
 ratings = random.randint(11, size = (num_movies, num_users))
 
-
 did_rate = (ratings != 0) * 1
 
-
-
 sheraz_ratings = zeros((num_movies, 1))
-
 
 sheraz_ratings[0] = 8
 sheraz_ratings[4] = 7
 sheraz_ratings[7] = 3
 
-
 ratings = append(sheraz_ratings, ratings, axis = 1)
 did_rate = append(((sheraz_ratings != 0) * 1), did_rate, axis = 1)
+
+pdb.set_trace()
 
 a = [10, 20, 30]
 aSum = sum(a)
@@ -51,6 +48,5 @@ def normalize_ratings(ratings, did_rate):
     return ratings_norm, ratings_mean
 
 ratings, ratings_mean = normalize_ratings(ratings, did_rate)
-
-print ratings
 print ratings_mean
+print ratings
